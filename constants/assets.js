77724be340fa8e9fe1e1478dfd6da9eaa81d538c
code-with-onye <onyemachitrust@gmail.com>
2022-05-04 17:30:52 -0700

    9jstore nft marketplace

---
+++ constants/assets.js
@@ -0,0 +1,41 @@
+import badge from "../assets/icons/badge.png";
+import eth from "../assets/icons/eth.png";
+import heart from "../assets/icons/heart.png";
+import left from "../assets/icons/left.png";
+import search from "../assets/icons/search.png";
+
+import logo from "../assets/images/logo.png";
+import nft01 from "../assets/images/nft01.png";
+import nft02 from "../assets/images/h-1.jpeg";
+import nft03 from "../assets/images/s-6.jpeg";
+import nft04 from "../assets/images/s-5.jpeg";
+import nft05 from "../assets/images/s-2.jpeg";
+import nft06 from "../assets/images/s-3.jpeg";
+import nft07 from "../assets/images/s-4.jpeg";
+
+import person01 from "../assets/images/person01.png";
+import person02 from "../assets/images/person02.png";
+import person03 from "../assets/images/person03.png";
+import person04 from "../assets/images/person04.png";
+
+export default {
+  badge,
+  eth,
+  heart,
+  left,
+  search,
+
+  logo,
+  nft01,
+  nft02,
+  nft03,
+  nft04,
+  nft05,
+  nft06,
+  nft07,
+
+  person01,
+  person02,
+  person03,
+  person04,
+};
